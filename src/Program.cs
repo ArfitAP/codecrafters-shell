64305f35ba5109a7a1884a27cf8a8067ef9ae28a
AntonioPavlis <antonio.pavlis51@gmail.com>
2026-09-18 20:20:12 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -7,6 +7,11 @@ class Program
             Console.Write("$ ");
             string? command = Console.ReadLine();
 
+            if (command == "exit")
+            {
+                break;
+            }
+
             Console.WriteLine($"{command}: command not found");
         }
     }
