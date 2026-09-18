0b8871c7eaacd66f9331e47ec0f66463c190c440
AntonioPavlis <antonio.pavlis51@gmail.com>
2026-09-18 20:29:57 +0200

    codecrafters submit [skip ci]

--- src/Program.cs
+++ src/Program.cs
@@ -16,6 +16,19 @@ class Program
                 Console.WriteLine(command[5..].Trim());
                 continue;
             }
+            else if (command != null && command.StartsWith("type "))
+            {
+                string commandName = command[5..].Trim();
+                if (commandName == "exit" || commandName == "echo" || commandName == "type")
+                {
+                    Console.WriteLine($"{commandName} is a shell builtin");
+                }
+                else
+                {
+                    Console.WriteLine($"{commandName}: not found");
+                }
+                continue;
+            }
 
             Console.WriteLine($"{command}: command not found");
         }
