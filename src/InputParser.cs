7e0c82c96badffadb68d04742d1acd82d3a6dab9
AntonioPavlis <antonio.pavlis51@gmail.com>
2026-09-24 19:25:33 +0200

    codecrafters submit [skip ci]

--- src/InputParser.cs
+++ src/InputParser.cs
@@ -12,16 +12,26 @@ namespace CodeCrafters.Shell.src
             StringBuilder currentArg = new StringBuilder();
             bool inSingleQuotes = false;
             bool inDoubleQuotes = false;
+            bool escapeNextChar = false;
             for (int i = 0; i < input.Length; i++)
             {
                 char c = input[i];
-                if (c == '\'' && !inDoubleQuotes)
+                if (escapeNextChar)
+                {
+                    currentArg.Append(c);
+                    escapeNextChar = false;
+                }
+                else if (c == '\'' && !inDoubleQuotes)
                 {
                     inSingleQuotes = !inSingleQuotes;
                 }
                 else if (c == '"')
                 {
                     inDoubleQuotes = !inDoubleQuotes;
+                }
+                else if(c == '\\' && !inSingleQuotes && !inDoubleQuotes)
+                {
+                    escapeNextChar = true;
                 }
                 else if (char.IsWhiteSpace(c) && !inSingleQuotes && !inDoubleQuotes)
                 {
